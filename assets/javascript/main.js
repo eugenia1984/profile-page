ba210dd3962fcebbecd33b678b3e4c387533a7ef
eugenia1984 <costamariaeugenia1@gmail.com>
2024-07-20 16:21:44 +0200

    feat: finalizo con el theme

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,7 +1,33 @@
 (() => {
-  const toggleTheme = () => {
-    console.log("toggle Theme");
+  // For the theme button
+  const themeButton = document.getElementById("theme");
+  const themeIcon = document.getElementById("light-dark-theme-button");
+  const htmlElement = document.querySelector("html");
+
+  // Function to set the theme
+  const setTheme = (theme) => {
+    if (theme === "dark") {
+      htmlElement.setAttribute("data-bs-theme", "dark");
+      themeIcon.setAttribute("src", "./assets/images/sol.svg");
+      localStorage.setItem("theme", "dark");
+    } else {
+      htmlElement.setAttribute("data-bs-theme", "light");
+      themeIcon.setAttribute("src", "./assets/images/luna.svg");
+      localStorage.setItem("theme", "light");
+    }
   };
 
-  toggleTheme(); 
+  // Check local storage for theme on load
+  const storedTheme = localStorage.getItem("theme");
+  if (storedTheme) {
+    setTheme(storedTheme);
+  } else {
+    setTheme("light"); 
+  }
+
+  // Theme toggle functionality
+  themeButton.addEventListener("click", () => {
+    const currentTheme = htmlElement.getAttribute("data-bs-theme");
+    (currentTheme === "light") ? setTheme("dark") : setTheme("light");
+  });
 })();
